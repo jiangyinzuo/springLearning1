36b38f5ee9bed94aaa3f92baf5deab37c0e24784
ChiangYintso <392711804@qq.com>
2019-11-07 20:12:15 +0800

    ch05: life circle of spring bean

--- src/main/java/pers/jiangyinzuo/learn/service/impl/AccountServiceImpl.java
+++ src/main/java/pers/jiangyinzuo/learn/service/impl/AccountServiceImpl.java
@@ -3,25 +3,43 @@ package pers.jiangyinzuo.learn.service.impl;
 import pers.jiangyinzuo.learn.service.AccountService;
 import pers.jiangyinzuo.learn.dao.AccountDao;
 
+import java.util.Date;
+
 public class AccountServiceImpl implements AccountService {
 
-    // private AccountDao accountDao = new AccountDaoImpl();
+    // 经常变化的数据不适用于依赖注入
+    private String name;
+    private Integer age;
+    private Date birthday;
+
     private static AccountDao accountDao;
 
-    public AccountServiceImpl() {
-        System.out.println("创建AccountServiceImpl对象");
+    public AccountServiceImpl() {}
+
+    public AccountServiceImpl(String name, Integer age, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public static void setAccountDao(AccountDao accountDao) {
+        AccountServiceImpl.accountDao = accountDao;
     }
 
     @Override
     public void saveAccount() {
-        System.out.println("存钱");
-    }
-
-    public void initMethod() {
-        System.out.println("init");
-    }
-
-    public void destroyMethod() {
-        System.out.println("destroy");
+        System.out.println("存钱" + name + age + birthday);
     }
 }
