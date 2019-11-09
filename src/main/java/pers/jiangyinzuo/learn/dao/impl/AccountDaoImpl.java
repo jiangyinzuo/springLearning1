9b75488a72d879b627b1d16c4798a925bb98a933
ChiangYintso <392711804@qq.com>
2019-11-09 14:29:05 +0800

    ch07: IOC based on annotation

--- src/main/java/pers/jiangyinzuo/learn/dao/impl/AccountDaoImpl.java
+++ src/main/java/pers/jiangyinzuo/learn/dao/impl/AccountDaoImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import pers.jiangyinzuo.learn.dao.AccountDao;
 import pers.jiangyinzuo.learn.domain.Account;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -15,6 +16,7 @@ import java.util.List;
 @Repository("accountDao")
 public class AccountDaoImpl implements AccountDao {
 
+    @Resource(name = "queryRunner")
     private QueryRunner runner;
 
     @Override
